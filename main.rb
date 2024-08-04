ebf97ce441f1166c53e884389c6ed2c74e716482
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-04 23:27:28 +0700

    Create methods for displaying game state and handling guesses

--- main.rb
+++ main.rb
@@ -11,10 +11,29 @@ class Hangman
     word_list.select { |word| word.length.between?(5, 12) }.sample
   end
 
-  def play
-    # TODO: Implement game logic
+  def display_game_state
+    puts "\nWord: #{display_word}"
+    puts "Incorrect guesses: #{@incorrect_guesses}/#{@max_guesses}"
+    puts "Guessed letters: #{@guessed_letters.join(', ')}"
+  end
+
+  def display_word
+    @word.chars.map { |char| @guessed_letters.include?(char) ? char : '_' }.join(' ')
+  end
+
+  def guessed_letter(letter)
+    letter.downcase!
+    return if @guessed_letters.include?(letter)
+
+    @guessed_letters << letter
+    if @word.include?(letter)
+      puts "Good guess!"
+    else
+      @incorrect_guesses += 1
+      puts "Sorry, that letter is not in the word."
+    end
   end
 end
 
 game = Hangman.new
-p game.select_word
+game.play
